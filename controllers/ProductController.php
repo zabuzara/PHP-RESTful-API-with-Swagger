73a2a52dfdf329c95e94594118c754dd1ee34998
OMID ME <zabuzara@yahoo.com>
2023-07-03 00:20:33 +0200

    echo class and method name

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -6,6 +6,6 @@ class ProductController {
 
     #[GetMapping('/get_by_id/{id}')]
     public function get_by_id(int $id) {
-
+        echo 'Method GET => '.__CLASS__.'->'.__FUNCTION__.'(id)';
     }
 }
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,31 +4,31 @@
 class UserController {
     #[GetMapping('/get_all')]
     public function get_all() {
-        echo 'GET used in '.__FUNCTION__;
+        echo 'Method GET => '.__CLASS__.'->'.__FUNCTION__.'()';
     }
 
     #[GetMapping('/get_by_id/{id}')]
     public function get_by_id(int $id) {
-        echo 'GET used in '.__FUNCTION__."(id)";
+        echo 'Method GET => '.__CLASS__.'->'.__FUNCTION__."(id)";
     }
 
     #[GetMapping('/get_by_name/{name}')]
     public function get_by_name(string $name) {
-        echo 'GET used in '.__FUNCTION__."(name)";
+        echo 'Method: GET => '.__CLASS__.'->'.__FUNCTION__."(name)";
     }
 
     #[PostMapping('/save_user')]
     public function save_user(array $user) {
-        echo 'POST used in '.__FUNCTION__."(user)";
+        echo 'Method: POST => '.__CLASS__.'->'.__FUNCTION__."(user)";
     }
 
     #[PutMapping('/update_user/{id}')]
     public function update_user(int $id, array $user) {
-        echo 'PUT used in '.__FUNCTION__.'(id, user)';
+        echo 'Method: PUT => '.__CLASS__.'->'.__FUNCTION__.'(id, user)';
     }
 
     #[DeleteMapping('/delete_user/{id}')]
     public function delete_user(int $id) {
-        echo 'DELETE used in '.__FUNCTION__.'(id)';
+        echo 'Method: DELETE => '.__CLASS__.'->'.__FUNCTION__.'(id)';
     }
 }
