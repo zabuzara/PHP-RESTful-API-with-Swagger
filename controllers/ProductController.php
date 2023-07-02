5fab1d3b44c84bcdadf0a2be451068f246aa34f7
OMID ME <zabuzara@yahoo.com>
2023-07-03 00:21:23 +0200

    adding get_all method

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -3,6 +3,10 @@
 #[Controller]
 #[RequestMapping('product')]
 class ProductController {
+    #[GetMapping('/get_all')]
+    public function get_all() {
+        echo 'Method GET => '.__CLASS__.'->'.__FUNCTION__.'()';
+    }
 
     #[GetMapping('/get_by_id/{id}')]
     public function get_by_id(int $id) {
