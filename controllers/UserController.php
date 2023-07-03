7737b49b6a980500094c1abc01652d6d315dfb1e
OMID ME <zabuzara@yahoo.com>
2023-07-03 13:12:20 +0200

    UserReposirory refactoring

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -11,51 +11,43 @@ class UserController {
 
     #[GetMapping('/get_all')]
     public function get_all() {
-        echo 'Method GET => '.__CLASS__.'->'.__FUNCTION__.'()';
         echo json_encode($this->repository->find_all(UserEntity::class));
     }
 
     #[GetMapping('/get_by_id/{id}')]
     public function get_by_id(int $id) {
-        echo 'Method GET => '.__CLASS__.'->'.__FUNCTION__."(id)";
         echo json_encode($this->repository->find_by_id(UserEntity::class, $id));
     }
 
     #[GetMapping('/get_by_forename/{forename}')]
     public function get_by_forename(string $forename) {
-        echo 'Method: GET => '.__CLASS__.'->'.__FUNCTION__."(forename)";
         echo json_encode($this->repository->find_by_forename(UserEntity::class, $forename));
     }
 
     #[GetMapping('/get_by_surname/{surname}')]
     public function get_by_surname(string $surname) {
-        echo 'Method: GET => '.__CLASS__.'->'.__FUNCTION__."(surname)";
         echo json_encode($this->repository->find_by_surname(UserEntity::class, $surname));
     }
 
     #[GetMapping('/get_by_email/{email}')]
     public function get_by_email(string $email) {
-        echo 'Method: GET => '.__CLASS__.'->'.__FUNCTION__."(email)";
         echo json_encode($this->repository->find_by_email(UserEntity::class, $email));
     }
 
     #[PostMapping('/save_user')]
     public function save_user(array $user) {
-        echo 'Method: POST => '.__CLASS__.'->'.__FUNCTION__."(user)";
         $entity = EntityManager::array_to_entity(UserEntity::class, $user);
         $this->repository->persist($entity);
     }
 
     #[PutMapping('/update_user/{id}')]
     public function update_user(int $id, array $user) {
-        echo 'Method: PUT => '.__CLASS__.'->'.__FUNCTION__.'(id, user)';
         $entity = EntityManager::array_to_entity(UserEntity::class, $user);
         $this->repository->update($entity, $id);
     }
 
     #[DeleteMapping('/delete_user/{id}')]
     public function delete_user(int $id) {
-        echo 'Method: DELETE => '.__CLASS__.'->'.__FUNCTION__.'(id)';
         $this->repository->remove(UserEntity::class, $id);
     }
 }
--- repositories/UserRepository.php
+++ repositories/UserRepository.php
@@ -2,39 +2,25 @@
 #[REPOSITORY]
 class UserRepository extends BaseRepository {
     public function find_by_email(string $entity_name, string $email) : array {
-        if ($this->table_exists($entity_name)) {
-            $table_name = $this->get_table_name($entity_name);
-            $primary_key_name = $this->get_primary_key($table_name);
-            $column_name = explode('find_by_', __FUNCTION__)[1];
-
-            if (!empty($primary_key_name)) {
-                return $this->get_context()->exec("SELECT * FROM `".$table_name."` WHERE `".$column_name."` LIKE ?", ["%".$email."%"]);
-            }
-        }
-        return [];
+        return $this->find($entity_name, $email, __FUNCTION__);
     }
 
     public function find_by_forename(string $entity_name, string $forename) : array {
-        if ($this->table_exists($entity_name)) {
-            $table_name = $this->get_table_name($entity_name);
-            $primary_key_name = $this->get_primary_key($table_name);
-            $column_name = explode('find_by_', __FUNCTION__)[1];
-
-            if (!empty($primary_key_name)) {
-                return $this->get_context()->exec("SELECT * FROM `".$table_name."` WHERE `".$column_name."` LIKE ?", ["%".$forename."%"]);
-            }
-        }
-        return [];
+        return $this->find($entity_name, $forename, __FUNCTION__);
     }
 
     public function find_by_surname(string $entity_name, string $surname) : array {
+        return $this->find($entity_name, $surname, __FUNCTION__);
+    }
+
+    private function find (string $entity_name, string $subject, $func) : array {
         if ($this->table_exists($entity_name)) {
             $table_name = $this->get_table_name($entity_name);
             $primary_key_name = $this->get_primary_key($table_name);
-            $column_name = explode('find_by_', __FUNCTION__)[1];
+            $column_name = explode('find_by_', $func)[1];
 
             if (!empty($primary_key_name)) {
-                return $this->get_context()->exec("SELECT * FROM `".$table_name."` WHERE `".$column_name."` LIKE ?", ["%".$surname."%"]);
+                return $this->get_context()->exec("SELECT * FROM `".$table_name."` WHERE `".$column_name."` LIKE ?", ["%".$subject."%"]);
             }
         }
         return [];
