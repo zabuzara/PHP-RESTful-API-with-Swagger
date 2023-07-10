c9c1bbf8ad2e50bc34ce8f8a3680330f593b2422
OMID ME <79631865+zabuzara@users.noreply.github.com>
2023-07-10 14:28:19 +0200

    Update UserController.php

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -14,22 +14,22 @@ class UserController {
     }
 
     #[GetMapping('/get_by_id/{id}')]
-    public function get_by_id(int $id) {
+    public function get_by_id(#[PathVariable(name: 'id', require: true, validate: Validate::INT)] int $id) {
         echo json_encode($this->repository->find_by_id(UserEntity::class, $id));
     }
 
     #[GetMapping('/get_by_forename/{forename}')]
-    public function get_by_forename(string $forename) {
+    public function get_by_forename(#[PathVariable(name: 'forename', require: true, validate: Validate::STRING)] string $forename) {
         echo json_encode($this->repository->find_by_forename(UserEntity::class, $forename));
     }
 
     #[GetMapping('/get_by_surname/{surname}')]
-    public function get_by_surname(string $surname) {
+    public function get_by_surname(#[PathVariable(name: 'surname', require: true, validate: Validate::STRING)] string $surname) {
         echo json_encode($this->repository->find_by_surname(UserEntity::class, $surname));
     }
 
     #[GetMapping('/get_by_email/{email}')]
-    public function get_by_email(string $email) {
+    public function get_by_email(#[PathVariable(name: 'email', require: true, validate: Validate::STRING)] string $email) {
         echo json_encode($this->repository->find_by_email(UserEntity::class, $email));
     }
 
@@ -40,13 +40,13 @@ class UserController {
     }
 
     #[PutMapping('/update_user/{id}')]
-    public function update_user(int $id, array $user) {
+    public function update_user(#[PathVariable(name: 'id', require: true, validate: Validate::INT)] int $id, array $user) {
         $entity = EntityManager::array_to_entity(UserEntity::class, $user);
         $this->repository->update($entity, $id);
     }
 
     #[DeleteMapping('/delete_user/{id}')]
-    public function delete_user(int $id) {
+    public function delete_user(#[PathVariable(name: 'id', require: true, validate: Validate::INT)] int $id) {
         $this->repository->remove(UserEntity::class, $id);
     }
 }
