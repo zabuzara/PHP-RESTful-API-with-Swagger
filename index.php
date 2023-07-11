3a11b21b1d67d1eeb42efcc9c6567f4f75ef4961
OMID ME <zabuzara@yahoo.com>
2023-07-11 08:37:04 +0200

    document_root changed

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ RESTful::with_authorization(
     session_key_for_token: 'token', 
     session_key_for_expiration: 'expiration_time'
 );
-new RESTful(document_root: '/PHP-Chat-API/', ignore_routes: ['security']);
+new RESTful(document_root: '/PHP-API-Template/', ignore_routes: ['security']);
