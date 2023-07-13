1d0581d2fc66dd39ee8be68de27a8062dd64b9e7
OMID ME <zabuzara@yahoo.com>
2023-07-13 19:08:54 +0200

    get_data

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -56,4 +56,34 @@ class UserController {
     public function delete_user(#[PathVariable(name: 'id', require: true, validate: Validate::INT)] int $id) {
         $this->repository->remove(UserEntity::class, $id);
     }
+
+    #[GetMapping('/get_data')]
+    public function get_data() {
+        $request_url = './RESTful/swagger/favicon.ico';
+        header('Content-Type: application/octet-stream');
+        header("Content-Transfer-Encoding: Binary"); 
+        header("Content-disposition: attachment; filename=\"" . basename($request_url) . ".ico\""); 
+        readfile($request_url);
+        exit();
+    }
+
+
+    #[PostMapping('/take_data')]
+    public function take_data(object $object) {
+        echo json_encode($object);
+        /* PUT data comes in on the stdin stream */
+        // $putdata = fopen("php://input", "r");
+
+        // /* Open a file for writing */
+        // $fp = fopen("myputfile.ext", "w");
+
+        // /* Read the data 1 KB at a time
+        // and write to the file */
+        // while ($data = fread($putdata, 1024))
+        // fwrite($fp, $data);
+
+        // /* Close the streams */
+        // fclose($fp);
+        // fclose($putdata);
+    }
 }
