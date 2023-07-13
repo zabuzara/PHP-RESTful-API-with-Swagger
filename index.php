e7a4556931af33c3a5ebae4e752dbc1c1d6009b7
OMID ME <zabuzara@yahoo.com>
2023-07-13 14:34:40 +0200

    done

--- index.php
+++ index.php
@@ -15,6 +15,5 @@ if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] === $domain_name) 
     if (count($document_root) > 0)
         $document_root = $document_root[1].'/';
 }
-echo 'Last Test ';
 new RESTful(document_root: $document_root , ignore_routes: ['security']);
 ?>
